95aa111010e4849ffe765d5072c5e702e2ac1094
martinBuquerin <martin.buquerin.elgue@gmail.com>
2022-10-05 09:39:09 +0200

    Update NewEmpleados.java

--- src/newempleados/NewEmpleados.java
+++ src/newempleados/NewEmpleados.java
@@ -35,8 +35,11 @@ public class NewEmpleados {
     }
     
         
-        imprimir(arrayEmpl);// usando la funcion imprimir con el array tipo persona
-        imprimir(numeros);// usando la funcion imprimir con el array tipo int
+       // imprimir(arrayEmpl);// usando la funcion imprimir con el array tipo persona
+       // imprimir(numeros);// usando la funcion imprimir con el array tipo int
+       // imprimirCantidad(numeros,1);
+        imprimirNumeros(3,1,21,23,23);//usando la funcion de imprimir numeros con parametro variable        
+        imprimirString("casimiro","leila","Jana");//usando la funcion de imprimir nombres con parametro variable
     }
     
      public static void imprimir(Object nombre[]){//funcion para imprimir cualquier array 
@@ -45,6 +48,32 @@ public class NewEmpleados {
         }
     }
    
+     
+    public static void imprimirCantidad(Object nombre[],int longitud){//funcion para imprimir cualquier array 
+        for (int i = 0; i < longitud; i++) {
+            System.out.print(nombre[i]+"\n");
+        }
+    }
+    
+    public static void imprimirNumeros(int... numeros){//funcion para imprimir una cantidad de parametros variable, la clave son los ...
+        
+        
+        for (int i = 0; i < numeros.length; i++) {
+           
+            System.out.print(numeros[i]+"\n");
+        }
+    }
+    
+        public static void imprimirString(String... nombres){//funcion para imprimir una cantidad de parametros variable, la clave son los ...
+        
+        
+        for (int i = 0; i < nombres.length; i++) {
+           
+            System.out.print(nombres[i]+"\n");
+        }
+    }
+    
+    
     public static int alea(int li, int ls){//funcion que devuelve numeros aleatorios
         return (int)((Math.round(Math.random()*(ls-li))+li));
     }
