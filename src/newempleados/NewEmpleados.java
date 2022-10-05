6fc6436e9f27f78193e2deae5394903564ffa076
martinBuquerin <martin.buquerin.elgue@gmail.com>
2022-10-05 09:04:39 +0200

    actualizado los comentarios

--- src/newempleados/NewEmpleados.java
+++ src/newempleados/NewEmpleados.java
@@ -17,35 +17,35 @@ public class NewEmpleados {
     
     
     public static void main(String[] args) {
-        Persona arrayEmpl[] = new Persona[3];
+        Persona arrayEmpl[] = new Persona[3];//nueva array de tipo Persona
        
-        Persona per1 = new Persona("martin","buquerin");
+        Persona per1 = new Persona("martin","buquerin");//creacion de objeto persona
         Persona per2 = new Persona("juian","loco");
         Persona per3 = new Persona("han","solo");
         
-        Integer numeros []= new Integer [3];
+        Integer numeros []= new Integer [3];//creacion de array tipo Int
 
-        arrayEmpl[0] = per1;
+        arrayEmpl[0] = per1;//asignacion de objeto persona al array  de tipo persona
         arrayEmpl[1] = per2;
         arrayEmpl[2] = per3;
         
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {//loop para completar automaticamente el array de tipo int de forma aleatoria
             numeros[i]=alea(0,9);
     }
     
         
-        imprimir(arrayEmpl);
-        imprimir(numeros);
+        imprimir(arrayEmpl);// usando la funcion imprimir con el array tipo persona
+        imprimir(numeros);// usando la funcion imprimir con el array tipo int
     }
     
-     public static void imprimir(Object nombre[]){
+     public static void imprimir(Object nombre[]){//funcion para imprimir cualquier array 
         for (int i = 0; i < nombre.length; i++) {
             System.out.print(nombre[i]+"\n");
         }
     }
    
-    public static int alea(int li, int ls){
+    public static int alea(int li, int ls){//funcion que devuelve numeros aleatorios
         return (int)((Math.round(Math.random()*(ls-li))+li));
     }
       
